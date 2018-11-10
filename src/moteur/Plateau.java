2cf1de8dcaab671ab6dabb19f1b3fa69d6dc373f
Christophe Peroumal Tevanin <christophe.peroumal35@gmail.com>
2018-11-10 17:56:06 +0100

    Ajout fichier xml + méthode load et save

--- src/moteur/Plateau.java
+++ src/moteur/Plateau.java
@@ -1,5 +1,22 @@
 package moteur;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.jdom2.Attribute;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.DOMBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+import org.xml.sax.SAXException;
+
+
 public class Plateau {
 
 	Jeton[][] plateau;
@@ -13,6 +30,9 @@ public class Plateau {
 		plateau = new Jeton[taillePlateau + 2][taillePlateau + 2];
 		this.taillePlateau = taillePlateau;
 		remplissageOut();
+	}
+	public Plateau () throws ParserConfigurationException, SAXException, IOException {
+		load("data/plateauX.xml");
 	}
 
 	public void remplissageOut() {
@@ -33,6 +53,7 @@ public class Plateau {
 	}
 
 	public void ajouterPion(Jeton unJeton, int x, int y, Orientation uneOrientation) {
+	
 		if (x == 0 || y == 0 || x == taillePlateau + 1 || y == taillePlateau - 1) {
 			plateau[x][y] = unJeton;
 			deplacement(unJeton, uneOrientation);
@@ -47,6 +68,21 @@ public class Plateau {
 		for (int i = 0; i < taillePlateau + 2; i++) {
 			for (int j = 0; j < taillePlateau + 2; j++) {
 				if (plateau[i][j] != null && !(plateau[i][j] instanceof Out)) {
+					System.out.println(plateau[i][j] + " à la position " + i + " " + j);
+				}
+			}
+		}
+	}
+	
+	public void afficherPlateauVide() {
+		System.out.println("\n\nVoici le plateau :");
+		for (int i = 0; i < taillePlateau + 2; i++) {
+			for (int j = 0; j < taillePlateau + 2; j++) {
+				if (plateau[i][j] == null) {
+					System.out.println(plateau[i][j] + " à la position " + i + " " + j);
+				}else if ((plateau[i][j] instanceof Out)) {
+					System.out.println(plateau[i][j] + " à la position " + i + " " + j);
+				}else if ((plateau[i][j] instanceof Rocher)) {
 					System.out.println(plateau[i][j] + " à la position " + i + " " + j);
 				}
 			}
@@ -83,7 +119,7 @@ public class Plateau {
 		if (directionDeplacement == Orientation.EST) {
 			int i = x;
 			i++;
-			// tant qu'il y a un jeton est qu'on ne déplace pas la taille du plateau
+			// tant qu'il y a un jeton et qu'on ne dépace pas la taille du plateau
 			while (i < taillePlateau + 1) {
 				// additionne la valeur de retour du jeton
 				// 0 si il n'a pas d'impacte 1 si il est dans notre sens et -1 si il est contre
@@ -237,4 +273,94 @@ public class Plateau {
 		}
 		return coordonne;
 	}
+	
+	// Sauvegarde des plateaux dans un fichier XML
+		// filename : nom du fichier
+		// comment : commentaire
+	public void save(String filename, String comment) throws ParserConfigurationException {
+		// On crée un nouveau Document JDOM
+		Document document = new Document();
+
+		// on set la racine du document avec l'élément meilleurScore
+		Element racine = new Element("Plateau");
+		document.setRootElement(racine);
+		
+		Attribute taille = new Attribute ("taille",""+taillePlateau);
+		racine.setAttribute(taille);
+
+		for (int x = 0; x < taillePlateau+2; x++) {
+			for (int y = 0; y < taillePlateau+2; y++) {
+			// on récupère les scores stockés dans la liste pour les enregister .
+
+			Element uneCase = new Element("Case");// balise score
+			racine.addContent(uneCase);
+			
+			if (plateau[x][y] instanceof Out) {
+				Attribute type1 = new Attribute("type","Out");
+			    uneCase.setAttribute(type1);
+			}else if(plateau[x][y] instanceof Rocher) {
+				Attribute type2 = new Attribute("type","Rocher");
+			    uneCase.setAttribute(type2);
+			}else{
+				Attribute type3 = new Attribute("type","In");
+			    uneCase.setAttribute(type3);
+			}
+			
+		      
+			Element horizontale = new Element("X");// balise valeur du score (nb de tentative)
+			uneCase.addContent(horizontale);
+			horizontale.setText(""+x);// on remplis les elements(balises) avec le contenu de la
+															// liste
+
+			Element verticale = new Element("Y");// balise pour savoir qui la réalisé
+			uneCase.addContent(verticale);
+			verticale.setText(""+y);
+			
+
+			}
+		}
+
+		try
+
+		{
+			XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
+			sortie.output(document, new FileWriter(filename));
+		} catch (IOException e) {
+		}
+	}
+	// Chargement des plateaux depuis un fichier XML
+		// filename : nom du fichier
+		public void load(String filename) throws ParserConfigurationException, SAXException, IOException {
+			// permet la création d'un fichier pour extraire les données du fichier xml
+			DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder documentBuilder = builderFactory.newDocumentBuilder();
+			org.w3c.dom.Document domDocument = documentBuilder.parse(filename);
+			DOMBuilder domBuilder = new DOMBuilder();
+			Document doc = domBuilder.build(domDocument);
+
+			// Element racine = doc.getRootElement();
+			// on recupère les fils de case a patir de la racine plateau
+			List<Element> attribElments = doc.getRootElement().getChildren("Case");
+			this.taillePlateau = Integer.parseInt(doc.getRootElement().getAttributeValue("taille")) ;
+			plateau = new Jeton[taillePlateau +2][taillePlateau+2];
+			// pour chaque case du fichier xml on écrit la case avec un x , un y et en focntion de ses attribut in , out , rien 
+			for (Element uneCase : attribElments) {
+				int x = Integer.parseInt(uneCase.getChildText("X")); 
+					int y = Integer.parseInt(uneCase.getChildText("Y")); 
+				if (uneCase.getAttributeValue("type").equals("Out")) {
+					plateau [x][y] = new Out();
+				}else if (uneCase.getAttributeValue("type").equals("In")){
+					plateau [x][y] = null;
+				}else if (uneCase.getAttributeValue("type").equals("Rocher")) {
+					int id = 1 ;
+					Rocher unRocher = new Rocher("cailloux "+id);
+					ajouterRocher(unRocher, x ,y);
+					id++;
+				
+				}
+			}
+
+		}
+	
+	
 }
